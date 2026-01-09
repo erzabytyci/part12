f9a3938d72ee5ec7009df39f716eebe86eed9bfe
erzabytyci <151072186+erzabytyci@users.noreply.github.com>
2026-01-09 22:27:32 +0200

    Exercise 12.22

--- my-app/frontend/src/services/datas.jsx
+++ my-app/frontend/src/services/datas.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = `${import.meta.env.VITE_BACKEND_URL}/persons`
+const baseUrl = `/api/persons`
 
 const getAll = () => {
     return axios.get(baseUrl).then(response => response.data.map(person => ({
